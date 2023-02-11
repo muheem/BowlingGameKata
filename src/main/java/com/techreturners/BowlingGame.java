2f03f6d33c13afa4089690d6a3cc8ec82d0c4979
ibrahim rahman <heemee@gmail.com>
2023-02-11 12:57:37 +0000

    4 tests pass. The spare stuff kinda works

--- src/main/java/com/techreturners/BowlingGame.java
+++ src/main/java/com/techreturners/BowlingGame.java
@@ -14,66 +14,63 @@ public class BowlingGame {
     private static final int TOTAL_ROUND = 10;
 
     private String[] scoreCard;
+    private int[] scoreCardSpares = {0,0,0,0,0,0,0,0,0,0,0,0};
     private int[] scoreValues = {0,0,0,0,0,0,0,0,0,0,0,0};
 
-    int calculateScore(String substring, int spares) {
+
+    private int GetSparesScore(int index) {
+        int value1 = 0;
+        int value2 = 0;
+        switch ( scoreCardSpares[index] ) {
+            case 2:
+                value2 = scoreValues[index + 2];
+                if ( value2 >= STRIKE_SCORE)
+                    value2 = STRIKE_SCORE;
+            // fall into case 1
+            case 1:
+                value1 = scoreValues[index + 2];
+                if ( value1 >= STRIKE_SCORE)
+                    value1 = STRIKE_SCORE;
+        }
+        return value1 + value2;
+    }
+
+    private int calculateScore(String substring, int index) {
         int score = 0;
 
         for (char c : substring.toCharArray()) {
             switch (c) {
                 case STRIKE:
-                    //score = STRIKE_SCORE;
-                    spares = 2;
+                    if (index < TOTAL_ROUND)
+                        scoreCardSpares[index]  = 2;
                     return STRIKE_SCORE;
                 case SPARE:
-                    spares = 1;
+                    if (index < TOTAL_ROUND)
+                        scoreCardSpares[index]  = 1;
                     return STRIKE_SCORE;
                 case MISS:
                     break;
                 default: // A number less than 10 (which would be a strike)
                     score += c - '0';
             }
-
         }
         return score;
     }
 
     public int totalScore(String scorecard) {
-
         int totalScore = 0;
-        int spares = 0;
 
         scoreCard = scorecard.split(" ");
-        System.out.println("len=" + scoreCard.length);
-
-        int currentScore = 0;
-        //for (String val: scoreCard) {
 
         for(int i = scoreCard.length - 1 ; i >= 0; i--) {
+            scoreValues[i] = calculateScore(scoreCard[i], i);
+            scoreValues[i] += GetSparesScore(i);
 
-            scoreValues[i] = calculateScore(scoreCard[i], spares);
-
-            // if spares = 1
-            if (spares == 2) {
-                scoreValues[i] += scoreValues[i] + scoreValues[i + 2];
-            }
-            if (spares > 0) {
-                scoreValues[i] += scoreValues[i] + scoreValues[i + 1];
-            }
-            totalScore += scoreValues[i];
+            // Ignore the spares at the end
+            if (i < TOTAL_ROUND)
+                totalScore += scoreValues[i];
+            System.out.println("totalScore=" + totalScore);
         }
-            //System.out.println(val);
-
-
-        // Calculate score
-
-        // PROBLEM OF THE SPARES
-
-
-
-        // Add up scores and return value
-
-        // Convert
 
         return totalScore;
     }
--- src/test/java/com/techreturners/BowlingGameTest.java
+++ src/test/java/com/techreturners/BowlingGameTest.java
@@ -6,34 +6,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BowlingGameTest {
 
-/*
-    @Test
-    public void CheckBowlingGameScoreAllStrikes() {
-        BowlingGame bowlingGame = new BowlingGame();
-
-        assertEquals( 300, bowlingGame.totalScore("X X X X X X X X X X X X") );
-    }
-
-    @Test
-    public void CheckBowlingGameScoreAGoodScore() {
-        BowlingGame bowlingGame = new BowlingGame();
-
-        assertEquals( 169, bowlingGame.totalScore("5/ 45  8/  X 0/ X 62 X 4/ X X") );
-    }
-*/
-
-
     @Test
     public void CheckBowlingGameScoreNoSpares() {
         BowlingGame bowlingGame = new BowlingGame();
-
-        assertEquals( 62, bowlingGame.totalScore("53 45  80  02 11 62 43 44 27 10") );
+        assertEquals(62, bowlingGame.totalScore("53 45  80  02 11 62 43 44 27 10"));
     }
 
     @Test
     public void CheckBowlingGameScoreWithOneSpare() {
         BowlingGame bowlingGame = new BowlingGame();
+        assertEquals(27, bowlingGame.totalScore("11 11 11 11 11 11 11 01 01 4/ 01"));
+    }
 
-        assertEquals( 27, bowlingGame.totalScore("11 11 11 11 11 11 11 01 01 4/ 01") );
+    @Test
+    public void CheckBowlingGameScoreAllStrikes() {
+        BowlingGame bowlingGame = new BowlingGame();
+        assertEquals(300, bowlingGame.totalScore("X X X X X X X X X X X X"));
+    }
+
+
+    @Test
+    public void CheckBowlingGameScoreAGoodScore() {
+        BowlingGame bowlingGame = new BowlingGame();
+        assertEquals(169, bowlingGame.totalScore("5/ 45  8/  X 0/ X 62 X 4/ X X"));
     }
 }
